7de3adf0477fff92917a7b357c3f919aabb53a56
Albert Mulia Shintra <albert.mulia@checkbox.ai>
2023-12-21 22:16:57 +0700

    chore: use scope path

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  
-  resources :class_rooms
+  scope '/api' do
+    resources :class_rooms
+  end
 end
